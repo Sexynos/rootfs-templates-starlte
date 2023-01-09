e3370d24185b17c6915155f5085a824003da03dc
Bardia Moshiri <fakeshell@bardia.tech>
2023-01-09 16:10:44 -0500

    add options to force no confirmation of config files

--- scripts/install-adaptation.sh
+++ scripts/install-adaptation.sh
@@ -50,7 +50,7 @@ curl -sS https://mirror.bardia.tech/exynos9810/exynos9810.gpg | tee /etc/apt/tru
 curl https://mirror.bardia.tech/exynos9810/exynos9810.gpg | sudo apt-key add -
 curl -sS -o /etc/apt/sources.list.d/exynos9810.list https://mirror.bardia.tech/exynos9810/exynos9810.list
 apt update
-DEBIAN_FRONTEND=noninteractive apt install -y adaptation-droidian-exynos9810 adaptation-exynos9810-configs
+DEBIAN_FRONTEND=noninteractive apt install -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y adaptation-droidian-exynos9810 adaptation-exynos9810-configs
 systemctl disable systemd-resolved systemd-timesyncd upower bluetooth
 systemctl mask systemd-resolved systemd-timesyncd upower bluetooth
 
